c18a48ba9268d0e693036656f9fc02786e77b0ce
shingo kubomura <skksb75@gmail.com>
2022-10-19 14:01:17 +0700

    create folder's seed data

--- laravel-project/database/seeders/DatabaseSeeder.php
+++ laravel-project/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use App\Models\Folder;
+use Database\Factories\UserFactory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +17,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $user = User::Factory()->create([
+            'name' => 'Shingo',
+            'email' => 'skksb75@gmail.com',
+        ]);
+        Folder::factory(5)->create([
+            'user_id' => $user->id
+        ]);
     }
 }
