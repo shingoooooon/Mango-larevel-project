e6aec23001df27ad97cf27a34867c53b36354770
shingo kubomura <skksb75@gmail.com>
2022-10-24 15:57:10 +0700

    refactor routes

--- laravel-project/routes/web.php
+++ laravel-project/routes/web.php
@@ -20,45 +20,50 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', function () {
-    return view('home');
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('/home', function () {
+        return view('home');
+    });
+
+    Route::get('/folders/{folder}/tasks', [TaskController::class, 'index'] )
+        ->name('tasks.index');
+
+    Route::get('/folders/{folder}/tasks/create', [TaskController::class, 'create'])
+        ->name('tasks.create');
+    Route::post('/folders/{folder}/tasks/store', [TaskController::class, 'store'])
+        ->name('tasks.store');
+
+    Route::get('/folders/{folder}/tasks/{task}/edit', [TaskController::class, 'edit'])
+        ->name('tasks.edit');
+    Route::patch('/folders/{folder}/tasks/{task}/update', [TaskController::class, 'update'])
+        ->name('tasks.update');
+
+    Route::delete('/folders/{folder}/tasks/{task}/delete', [TaskController::class, 'destroy'])
+        ->name('tasks.destroy');
+
+    Route::get('/folders/create', [FolderController::class, 'create'])
+        ->name('folders.create');
+    Route::post('/folders/store', [FolderController::class, 'store'])
+        ->name('folders.store');
+
+    Route::post('/logout', [UserController::class, 'logout']);
 });
 
-Route::get('/folders/{folder}/tasks', [TaskController::class, 'index'] )
-    ->middleware('auth')
-    ->name('tasks.index');
-
-Route::get('/folders/{folder}/tasks/create', [TaskController::class, 'create'])
-    ->middleware('auth')
-    ->name('tasks.create');
-Route::post('/folders/{folder}/tasks/store', [TaskController::class, 'store'])
-    ->middleware('auth')
-    ->name('tasks.store');
-
-Route::get('/folders/{folder}/tasks/{task}/edit', [TaskController::class, 'edit'])
-    ->middleware('auth')
-    ->name('tasks.edit');
-Route::patch('/folders/{folder}/tasks/{task}/update', [TaskController::class, 'update'])
-    ->middleware('auth')
-    ->name('tasks.update');
-
-Route::delete('/folders/{folder}/tasks/{task}/delete', [TaskController::class, 'destroy'])
-    ->middleware('auth')
-    ->name('tasks.destroy');
+Route::group(['middleware' => 'guest'], function() {
+    Route::get('/register', [UserController::class, 'create']);
+    Route::post('/users', [UserController::class, 'store']);
+    Route::get('/login', [UserController::class, 'login'])
+        ->name('login');
+    Route::post('/authenticate', [UserController::class, 'authenticate']);
+});
 
 
-Route::get('/folders/create', [FolderController::class, 'create'])
-    ->middleware('auth')
-    ->name('folders.create');
-Route::post('/folders/store', [FolderController::class, 'store'])
-    ->middleware('auth')
-    ->name('folders.store');
 
-Route::get('/register', [UserController::class, 'create'])
-    ->middleware('guest');
-Route::post('/users', [UserController::class, 'store']);
-Route::get('/login', [UserController::class, 'login'])
-    ->middleware('guest');
-Route::post('/authenticate', [UserController::class, 'authenticate']);
-Route::post('/logout', [UserController::class, 'logout'])
-    ->middleware('auth');
+
+
+
+
+
+
+
+
