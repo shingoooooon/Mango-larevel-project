916523376dd6a9eaab443dddfda4aebe3a78af7c
shingo kubomura <skksb75@gmail.com>
2022-10-24 14:22:18 +0700

    add middleware auth and guest

--- laravel-project/routes/web.php
+++ laravel-project/routes/web.php
@@ -25,29 +25,40 @@ Route::get('/home', function () {
 });
 
 Route::get('/folders/{folder}/tasks', [TaskController::class, 'index'] )
+    ->middleware('auth')
     ->name('tasks.index');
 
 Route::get('/folders/{folder}/tasks/create', [TaskController::class, 'create'])
+    ->middleware('auth')
     ->name('tasks.create');
 Route::post('/folders/{folder}/tasks/store', [TaskController::class, 'store'])
+    ->middleware('auth')
     ->name('tasks.store');
 
 Route::get('/folders/{folder}/tasks/{task}/edit', [TaskController::class, 'edit'])
+    ->middleware('auth')
     ->name('tasks.edit');
 Route::patch('/folders/{folder}/tasks/{task}/update', [TaskController::class, 'update'])
+    ->middleware('auth')
     ->name('tasks.update');
 
 Route::delete('/folders/{folder}/tasks/{task}/delete', [TaskController::class, 'destroy'])
+    ->middleware('auth')
     ->name('tasks.destroy');
 
 
 Route::get('/folders/create', [FolderController::class, 'create'])
+    ->middleware('auth')
     ->name('folders.create');
 Route::post('/folders/store', [FolderController::class, 'store'])
+    ->middleware('auth')
     ->name('folders.store');
 
-Route::get('/register', [UserController::class, 'create']);
+Route::get('/register', [UserController::class, 'create'])
+    ->middleware('guest');
 Route::post('/users', [UserController::class, 'store']);
-Route::get('/login', [UserController::class, 'login']);
+Route::get('/login', [UserController::class, 'login'])
+    ->middleware('guest');
 Route::post('/authenticate', [UserController::class, 'authenticate']);
-Route::post('/logout', [UserController::class, 'logout']);
+Route::post('/logout', [UserController::class, 'logout'])
+    ->middleware('auth');
