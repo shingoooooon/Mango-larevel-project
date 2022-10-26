43e366ef706969a4f1a141d51125bd6fdd84a4fd
shingo kubomura <skksb75@gmail.com>
2022-10-26 15:57:34 +0700

    style register-login page

--- laravel-project/resources/views/users/register.blade.php
+++ laravel-project/resources/views/users/register.blade.php
@@ -1,14 +1,14 @@
 <x-layout>
-    <div class="container border mx-auto p-6 mt-6">
+    <div class="container border mx-auto w-1/3 p-6 mt-32">
         <header class="text-center">
-            <h2 class="text-2xl font-bold uppercase mb-1">
+            <h2 class="text-2xl font-bold uppercase mb-3">
                 Register
             </h2>
-            <p class="mb-4">Create an account to make your TODO list</p>
+            <p class="mb-10">Create an account to make your TODO list</p>
         </header>
         <form method="post" action="/storeuser">
             @csrf
-            <div class="relative z-0 mb-6 w-full group">
+            <div class="relative z-0 mb-10 w-full group">
                 <input type="text" name="name" id="name" value="{{ old('name') }}" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                 <label for="name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Name</label>
                 @error('name')
@@ -16,7 +16,7 @@
                 @enderror
             </div>
 
-            <div class="relative z-0 mb-6 w-full group">
+            <div class="relative z-0 mb-10 w-full group">
                 <input type="email" name="email" id="email" value="{{ old('email') }}" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                 <label for="email" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email</label>
                 @error('email')
@@ -24,7 +24,7 @@
                 @enderror
             </div>
 
-            <div class="relative z-0 mb-6 w-full group">
+            <div class="relative z-0 mb-10 w-full group">
                 <input type="password" name="password" id="password" value="{{ old('password') }}" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                 <label for="password" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
                 @error('password')
@@ -32,17 +32,20 @@
                 @enderror
             </div>
 
-            <div class="relative z-0 mb-6 w-full group">
+            <div class="relative z-0 mb-10 w-full group">
                 <input type="password" name="password_confirmation" id="password_confirmation" value="{{ old('password_confirmation') }}" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                 <label for="password_confirmation" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Confirm Password</label>
                 @error('password_confirmation')
                 <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
                 @enderror
             </div>
-            <button type="submit" class="text-white bg-teal-700 hover:bg-teal-800 focus:ring-4 focus:outline-none focus:ring-teal-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-teal-600 dark:hover:bg-teal-700 dark:focus:ring-teal-800">Create Account</button>
+            <div class="text-center">
+                <button type="submit" class="text-white bg-teal-700 hover:bg-teal-800 focus:ring-4 focus:outline-none focus:ring-teal-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-teal-600 dark:hover:bg-teal-700 dark:focus:ring-teal-800">Register</button>
+            </div>
             <p class="mt-8">Already have an account?
-                <a href="/login" class="text-teal-600">Login</a>
+                <a href="/login" class="text-teal-600"> Login</a>
             </p>
         </form>
     </div>
 </x-layout>
+
--- laravel-project/resources/views/welcome.blade.php
+++
@@ -1,3 +0,0 @@
-<x-layout>
-    Home
-</x-layout>
