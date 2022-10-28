6cb7858cbc5a393b148a0a1661e1564bcebb7473
shingo kubomura <skksb75@gmail.com>
2022-10-28 10:06:22 +0700

    add dropdown

--- laravel-project/resources/views/partials/_navigation.blade.php
+++ laravel-project/resources/views/partials/_navigation.blade.php
@@ -11,9 +11,11 @@
     <div id="dropdown" class="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700">
         <ul class="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefault">
             <li>
+                @if(auth()->user()->folders()->first())
                 <a href="{{ route('tasks.index', ['folder' => auth()->user()->folders()->first()]) }}" class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
                     <i class="fas fa-columns mr-1"></i>Dashboard
                 </a>
+                @endif
             </li>
             <li>
             @if(auth()->user()->is_admin)
