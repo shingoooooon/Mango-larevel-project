988270fbdb59e7d2a3cec70268c9950a6de35cb8
shingo kubomura <skksb75@gmail.com>
2022-10-30 16:13:26 +0700

    add profile page

--- laravel-project/resources/views/users/show.blade.php
+++ laravel-project/resources/views/users/show.blade.php
@@ -1,17 +1,17 @@
 <x-layout>
     @include('partials._navigation')
-    <div class="container mx-auto">
-        <div class="p-4 w-full bg-gray-50 mt-6 rounded">
-            <div class="text-center border-b border-gray-200">
-                <h1 class="text-xl font-bold my-10">Profile</h1>
-                <a href="{{ route('users.editprofile', ['user' => $user->id]) }}">
-                    <i class="fas fa-edit hover:text-gray-400"></i>
-                </a>
-            </div>
+    <div class="container mx-auto w-4/5 bg-gray-50 mt-6 p-6 rounded flex items-center justify-around">
+        <div class="p-5">
+            <h1 class="mb-8 text-5xl text-center">{{ $user->username }}</h1>
+            <h2 class="mb-8 align-middle"><i class="fas fa-envelope text-2xl mr-5"></i> {{ $user->email }}</h2>
+            <h2 class="mb-8"><i class="fas fa-key text-2xl mr-5"></i> {{ $user->password }}</h2>
+            <h2 class="mb-8"><i class="fas fa-user-cog text-2xl mr-5"></i> {{ $user->admin_label }}</h2>
+            <a href="{{ route('users.editprofile', ['user' => $user->id]) }}" class="text-white bg-teal-500 hover:bg-teal-700 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none">
+                <i class="fas fa-edit"><span class="font-sans"> Edit Profile</span></i>
+            </a>
+        </div>
+        <div class="text-center border-b border-gray-200 p-5">
             <img src="{{ $user->icon ? asset('storage/' . $user->icon) : asset('images/icon.png') }}" alt="icon" class="md-block w-48">
-            <p>{{ $user->username }}</p>
-            <p>{{ $user->email }}</p>
-            <p>{{ $user->password }}</p>
         </div>
     </div>
 </x-layout>
