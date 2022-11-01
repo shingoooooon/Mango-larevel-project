1003475c5a3ddc84cb37944e6efde6f3cb9a843e
shingo kubomura <skksb75@gmail.com>
2022-11-01 14:05:28 +0700

    organize script files

--- laravel-project/resources/views/components/layout.blade.php
+++ laravel-project/resources/views/components/layout.blade.php
@@ -6,6 +6,10 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://unpkg.com/flowbite@1.5.3/dist/flowbite.js"></script>
+    <script src="https://unpkg.com/flowbite@1.5.3/dist/datepicker.js"></script>
+    <script src="//unpkg.com/alpinejs" defer></script>
+    <script src="https://cdn.tailwindcss.com"></script>
     <title>TODO</title>
 </head>
 <body>
@@ -13,9 +17,5 @@
     {{ $slot }}
 </main>
 <x-flash-message></x-flash-message>
-<script src="https://unpkg.com/flowbite@1.5.3/dist/flowbite.js"></script>
-<script src="https://unpkg.com/flowbite@1.5.3/dist/datepicker.js"></script>
-<script src="//unpkg.com/alpinejs" defer></script>
-<script src="https://cdn.tailwindcss.com"></script>
 </body>
 </html>
