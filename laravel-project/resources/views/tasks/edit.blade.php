547f3cf9af798d30093ed3b66fae276877ef001c
shingo kubomura <skksb75@gmail.com>
2022-11-02 13:35:52 +0700

    add lavel for duedate

--- laravel-project/resources/views/tasks/edit.blade.php
+++ laravel-project/resources/views/tasks/edit.blade.php
@@ -13,7 +13,7 @@
                 @enderror
             </div>
 
-            <div class="relative z-0 mb-6 w-full group">
+            <div class="relative z-0 mb-8 w-full group">
                 <label for="status" class="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Status</label>
                 <select name="status" id="status" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required>
                 @foreach(\App\Models\Task::STATUS as $key => $val)
@@ -30,6 +30,7 @@
                         <svg aria-hidden="true" class="w-5 h-5 text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
                     </div>
                     <input datepicker datepicker-autohide name="due_date" type="text" value="{{ old('due_date') ?? $task->due_date }}" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5" placeholder="Select date">
+                    <label for="Due Date" class="top-0 peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Due Date</label>
                 </div>
                 @error('due_date')
                 <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
