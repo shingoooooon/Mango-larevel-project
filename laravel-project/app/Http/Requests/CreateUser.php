75ff6775d59353432b9d06a141fa7c0d6b06e9f0
shingo kubomura <skksb75@gmail.com>
2022-11-02 12:34:45 +0700

    add validation

--- laravel-project/app/Http/Requests/CreateUser.php
+++ laravel-project/app/Http/Requests/CreateUser.php
@@ -24,7 +24,7 @@ class CreateUser extends FormRequest
     public function rules()
     {
         return [
-            'username' => 'required|min:3',
+            'username' => 'required|min:3|unique:users',
             'email' => 'required|email|unique:users',
             'password' => 'required|confirmed|min:6'
         ];
